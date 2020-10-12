ea14d90118ee6bbd90f05078577fe089ecae0a1c
Arthur Lima <arthur-esc@hotmail.com>
2020-10-12 18:44:19 -0300

    feat: add signup page

--- src/app/modules/acesso/acesso.module.ts
+++ src/app/modules/acesso/acesso.module.ts
@@ -7,6 +7,7 @@ import { EsqueciMinhaSenhaComponent } from './esqueci-minha-senha/esqueci-minha-
 import { SharedModule } from '../shared/shared.module';
 import { RedefinirSenhaComponent } from './redefinir-senha/redefinir-senha.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   imports: [CommonModule, AcessoRoutingModule, SharedModule, FontAwesomeModule],
@@ -14,6 +15,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     EsqueciMinhaSenhaComponent,
     LoginComponent,
     RedefinirSenhaComponent,
+    CadastroComponent,
   ],
 })
 export class AcessoModule {}
