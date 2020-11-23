1c6be61357101a53425329335594b701678233cc
Arthur Lima <arthur-esc@hotmail.com>
2020-11-23 02:19:09 -0300

    feat: filtering scheduling by day and picker colors

--- src/app/modules/parceiro/agenda/agenda.component.ts
+++ src/app/modules/parceiro/agenda/agenda.component.ts
@@ -1,11 +1,15 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { DatabaseService } from '../../shared/services/database.service';
+import { ModalAgendaComponent } from '../../shared/components/modal-agenda/modal-agenda.component';
 import {
   AGENDAMENTO,
   STATUS_AGENDAMENTO,
 } from '../../shared/constants/agendamentos';
+
+const DAY_NAMES = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
 
 @Component({
   selector: 'app-agenda',
@@ -13,32 +17,46 @@ import {
   styleUrls: ['./agenda.component.scss'],
 })
 export class AgendaComponent implements OnInit {
-  @ViewChild('calendar') calendar: ElementRef;
-
   iconCalendar = faCalendarAlt;
   atendimentos = [];
   menuSelected = STATUS_AGENDAMENTO.ABERTO;
   statusOfService = ['Abertos', 'Confirmados', 'Encerrados'];
+  selectedDate = new Date();
 
-  constructor(private router: Router, private database: DatabaseService) {}
+  constructor(
+    private router: Router,
+    private database: DatabaseService,
+    private modalService: NgbModal
+  ) {}
 
   ngOnInit(): void {
     this.loadList();
   }
 
-  loadList() {
+  loadList(date = new Date()) {
+    const filterDate = date.toLocaleDateString('pt');
     this.atendimentos = this.database
       .list<AGENDAMENTO[]>('agendamentos')
-      .filter(({ status }) => status === this.menuSelected);
+      .filter(({ status, data }) => status === this.menuSelected && data === filterDate);
   }
 
   openCalendar() {
-    this.calendar.nativeElement.click();
+    const modalRef = this.modalService.open(ModalAgendaComponent);
+    modalRef.componentInstance.onlyDay = true;
+    modalRef.componentInstance.setHourDay.subscribe(({ date }) => {
+      this.selectedDate = date;
+      this.loadList(this.selectedDate);
+      modalRef.close();
+    });
   }
 
   changeMenu(index: number) {
     this.menuSelected = index;
     this.loadList();
+  }
+
+  getSelectedDateLabel() {
+    return `${this.selectedDate.toLocaleDateString('pt')} - ${DAY_NAMES[this.selectedDate.getDay()]}`;
   }
 
   abrirDetalhes(agendaId) {
